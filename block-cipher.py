0af3d286941269df5276ddc383af3aa9629119d8
martingrzzler <martin.gressler@code.berlin>
2022-01-26 11:26:19 +0100

    diffie hellman

--- block-cipher.py
+++ block-cipher.py
@@ -8,4 +8,7 @@ k = des(key, ECB, iv, pad=None, padmode=PAD_PKCS5)
 
 cipher = k.encrypt(message)
 
+# you divide message into fixed size blocks and padd the end
 # altering one block makes the whole block unreadable
+# double des can be brute forced with linear time
+# triple des can't
---
+++ key-exchange.py
@@ -0,0 +1,73 @@
+from distutils.log import error
+import math
+import random
+
+def is_prime(p):
+	for i in range(2, math.isqrt(p) + 1):
+		if p % i == 0:
+			return False
+	return True
+
+def rand_prime(start: int):
+
+	while True:
+		p = random.randrange(start, 2*start)
+		if is_prime(p):
+			return p
+
+def is_generator(g, p):
+	for i in range(2, p - 1):
+		if (g**i) % p == 1:
+			return False
+	return True
+
+def get_generator(p):
+	for g in range(2, p):
+		if is_generator(g, p):
+			return g
+
+def make_generator(p):
+	for g in range(2, p):
+		s = set()
+		is_gen = True
+		for i in range(2, p):
+			val = (g**i) % p
+			if val in s:
+				is_gen = False
+				break
+			s.add(val)
+		if is_gen:
+			return g
+
+
+# Public
+p = rand_prime(10000)
+g = make_generator(p)
+if g == None:
+	raise error("No generator found")
+
+
+print(f"p={p}, g={g}")
+
+# Alice
+a = random.randrange(0, p)
+g_a = (g**a) % p
+
+# Bob
+b = random.randrange(0, p)
+g_b = (g**b) % p
+
+# Alice
+alice_key = (g_b**a) % p
+
+# Bob
+bob_key = (g_a**b) % p
+
+print(f"Alice's key: {alice_key}, Bob's key: {bob_key}")
+
+
+
+
+
+
+
